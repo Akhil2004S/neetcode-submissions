1478cadd54f7ffb0a6160411664e529fc304e655
S Akhil <126699404+Akhil2004S@users.noreply.github.com>
2026-06-24 13:33:49 +0530

    Add: three-integer-sum - submission-1

---
+++ Data Structures & Algorithms/three-integer-sum/submission-1.go
@@ -0,0 +1,46 @@
+func threeSum(nums []int) [][]int {
+		for i := range nums {
+		for j := i; j > 0 && nums[j-1] > nums[j]; j-- {
+			nums[j], nums[j-1] = nums[j-1], nums[j]
+		}
+	}
+
+	var triplets [][]int
+	for i, num := range nums {
+		if i != 0 && nums[i] == nums[i-1] {
+			continue
+		}
+		arrayToLook := nums[i+1:]
+		target := -num
+		left := 0
+		right := len(arrayToLook) - 1
+		for left < right {
+			sum := arrayToLook[left] + arrayToLook[right]
+			if sum == target {
+				triplet := []int{num, arrayToLook[left], arrayToLook[right]}
+				triplets = append(triplets, triplet)
+				left++
+				right--
+
+				for left < right && arrayToLook[left] == arrayToLook[left-1] {
+					left++
+				}
+
+				for left < right && arrayToLook[right] == arrayToLook[right+1] {
+					right--
+				}
+			}
+			for left < right && sum > target {
+				right--
+				break
+			}
+
+			for left < right && sum < target {
+				left++
+				break
+			}
+		}
+	}
+
+	return triplets
+}
