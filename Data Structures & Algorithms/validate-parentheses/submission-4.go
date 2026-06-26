47d6f6919206889a61547bb5127ecf541aa01c95
S Akhil <126699404+Akhil2004S@users.noreply.github.com>
2026-06-26 12:24:07 +0530

    Add: validate-parentheses - submission-4

---
+++ Data Structures & Algorithms/validate-parentheses/submission-4.go
@@ -0,0 +1,26 @@
+func isValid(s string) bool {
+    	var stack []rune
+	if len(s)%2 != 0 {
+		return false
+	}
+	for _, char := range s {
+		if char == '}' || char == ']' || char == ')' {
+			if len(stack) == 0 {
+				// Underflow
+				return false
+			}
+			top := stack[len(stack)-1]
+			if (top == '{' && char == '}') || (top == '(' && char == ')') || (top == '[' && char == ']') {
+				stack = stack[:len(stack)-1]
+				continue
+			} else {
+				return false
+			}
+		}
+		stack = append(stack, char)
+	}
+	if len(stack) != 0 {
+		return false
+	}
+	return true
+}
